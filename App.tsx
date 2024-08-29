bfbfa2c9d5fb0c3cdac0c538d980a85f78bd8d3d
Rafal <rgrajcar@gmail.com>
2024-08-29 22:21:18 +0200

    added Dashboard to App component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import {StyleSheet} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import {Dashboard} from "./src/screens/Dashboard";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <StatusBar />
-      <SafeAreaView></SafeAreaView>
+      <SafeAreaView>
+        <Dashboard />
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
