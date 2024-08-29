9d1d558c2b15a2c894758b6d8f9fcca8d411ac06
Rafal <rgrajcar@gmail.com>
2024-08-29 22:21:05 +0200

    added first screen Dashboard component

---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,11 @@
+import {StyleSheet, View, Text} from "react-native";
+
+export const Dashboard = () => {
+  return (
+    <View>
+      <Text>Dashboard</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
