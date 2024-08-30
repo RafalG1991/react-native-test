f85e012e0ada98c93d8b9b7f4c227c0901f244f1
Rafal <rgrajcar@gmail.com>
2024-08-30 16:30:24 +0200

    added styles to each component's element

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -3,12 +3,25 @@ import Feather from '@expo/vector-icons/Feather';
 
 export const Dashboard = () => {
   return (
-    <View>
-      <Text>Warszawa</Text>
-      <Text>22° C</Text>
-      <Feather name="sun" size={24} color="black" />
+    <View style={styles.container}>
+      <Text style={styles.cityName}>Warszawa</Text>
+      <Text style={styles.temperature}>22° C</Text>
+      <Feather name="sun" size={100} color="#fcd303" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  cityName: {
+    fontSize: 30,
+    color: "white",
+  },
+  temperature: {
+    fontSize: 50,
+    color: "white",
+  },
+
+});
