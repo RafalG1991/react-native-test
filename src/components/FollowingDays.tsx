f6322b745271b973c66cc929e4e85745564720e4
Rafal <rgrajcar@gmail.com>
2024-09-01 21:51:15 +0200

    added new FollowingDays component

---
+++ src/components/FollowingDays.tsx
@@ -0,0 +1,11 @@
+import {StyleSheet, Text, View} from "react-native";
+
+export const FollowingDays = () => {
+  return (
+    <View>
+      <Text>Day</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
