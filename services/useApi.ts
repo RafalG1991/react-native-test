ebbcfeb5518620e65a270089aaf8d91e1c0142f5
Rafal <rgrajcar@gmail.com>
2024-09-02 20:45:52 +0200

    changed language of API data

--- services/useApi.ts
+++ services/useApi.ts
@@ -4,7 +4,7 @@ export const useApi = () => {
   const [current, setCurrent] = useState<any>(null);
 
   useEffect(() => {
-    fetch(`${process.env.EXPO_PUBLIC_API_URL}/current.json?key=${process.env.EXPO_PUBLIC_KEY}&q=Czestochowa`)
+    fetch(`${process.env.EXPO_PUBLIC_API_URL}/current.json?key=${process.env.EXPO_PUBLIC_KEY}&q=Częstochowa&lang=PL`)
       .then(res => res.json())
       .then(res => setCurrent(res));
   }, []);
