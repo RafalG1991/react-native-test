057a0122f18de8bedfc96f1a69b055bd8c300bec
Rafal <rgrajcar@gmail.com>
2024-09-03 20:13:17 +0200

    added NavigationContainer to App and installed native stack package

--- App.tsx
+++ App.tsx
@@ -4,16 +4,19 @@ import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import {Dashboard} from "./src/screens/Dashboard";
 import {COLORS} from "./src/themes/colors";
 import {DayDetails} from "./src/screens/DayDetails";
+import {NavigationContainer} from "@react-navigation/native";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar style={"light"}/>
-      <SafeAreaView style={styles.container}>
-        {/*<Dashboard />*/}
-        <DayDetails />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <StatusBar style={"light"}/>
+        <SafeAreaView style={styles.container}>
+          {/*<Dashboard />*/}
+          <DayDetails />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
 
