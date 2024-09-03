67f83b6a7b5fdfc0549b2e90907acbc7e1a06711
Rafal <rgrajcar@gmail.com>
2024-09-03 20:00:12 +0200

    added DayDetails component to display hourly weather forecast for chosen day

--- App.tsx
+++ App.tsx
@@ -3,13 +3,15 @@ import {StyleSheet} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import {Dashboard} from "./src/screens/Dashboard";
 import {COLORS} from "./src/themes/colors";
+import {DayDetails} from "./src/screens/DayDetails";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <StatusBar style={"light"}/>
       <SafeAreaView style={styles.container}>
-        <Dashboard />
+        {/*<Dashboard />*/}
+        <DayDetails />
       </SafeAreaView>
     </SafeAreaProvider>
   );
---
+++ src/screens/DayDetails.tsx
@@ -0,0 +1,73 @@
+import {useWeatherApiForecast} from "../../services/useWeatherApiForecast";
+import {ActivityIndicator, Image, ScrollView, StyleSheet, Text, View} from "react-native";
+import {COLORS} from "../themes/colors";
+
+export const DayDetails = () => {
+  const forecast = useWeatherApiForecast();
+
+  if(!forecast) return <ActivityIndicator color={COLORS.sun} size="large" style={{height: '100%'}}/>
+
+  const day = forecast[0].hour;
+
+  return (
+    <ScrollView>
+        <View style={styles.followingDaysContainer}>
+          <Text style={[styles.content, styles.title]}>Pogoda w dniu {forecast[0].date}</Text>
+          {day.map((item, index) => (
+            <View style={[styles.container, !(index === day.length-1) && styles.separator]}>
+              <Text style={styles.content}>{item.time.split(' ')[1]}</Text>
+              <Text style={[styles.content, styles.value]}>{item.temp_c}°C</Text>
+              <Image
+                style={styles.weatherIcon}
+                source={{uri: `http:${item.condition.icon}`,}}
+                resizeMode="contain"
+                width={64}
+                height={64}
+              />
+            </View>
+          ))}
+        </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    fontWeight: '600',
+    fontSize: 18,
+  },
+  weatherIcon: {
+    width: 64,
+    height: 64,
+  },
+  followingDaysContainer: {
+    width: "90%",
+    margin: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: COLORS.lightBlue,
+    borderRadius: 10,
+  },
+  container: {
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  separator: {
+    borderBottomWidth: 1,
+    borderColor: COLORS.background,
+  },
+  content: {
+    flex: 1,
+    color: COLORS.text,
+  },
+  value: {
+    fontWeight: "600",
+  },
+  type: {
+    textAlign: "right",
+    color: COLORS.sun,
+  },
+});
