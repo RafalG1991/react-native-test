8d8e0fa566bf56edf8469f8196ba13dc31e72f8a
Rafal <rgrajcar@gmail.com>
2024-09-04 20:16:39 +0200

    added TextInput element with basic styling

--- src/screens/SelectLocation.tsx
+++ src/screens/SelectLocation.tsx
@@ -1,15 +1,22 @@
-import {TouchableOpacity, View, Text} from "react-native";
+import {View, TextInput, StyleSheet} from "react-native";
 import {useNavigation} from "@react-navigation/native";
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
 import {RootStackParamList} from "../navigation/Root";
+import {COLORS} from "../themes/colors";
 
 export const SelectLocation = () => {
   const { navigate } = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   return (
     <View>
-      <TouchableOpacity onPress={() => navigate('Dashboard')}>
-        <Text>SelectLocation</Text>
-      </TouchableOpacity>
+      <TextInput style={styles.input}/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  input: {
+    width: "100%",
+    height: 50,
+    backgroundColor: COLORS.lightBlue,
+  },
+});
