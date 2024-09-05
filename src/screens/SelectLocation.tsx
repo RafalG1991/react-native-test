7be1b415dd9e6892d46aeea20823453d6f3533ce
Rafal <rgrajcar@gmail.com>
2024-09-05 18:55:29 +0200

    added FlatList component, moved elements to new component with styling

--- src/screens/SelectLocation.tsx
+++ src/screens/SelectLocation.tsx
@@ -1,9 +1,10 @@
-import {View, TextInput, StyleSheet, TouchableOpacity, Text} from "react-native";
+import {View, TextInput, StyleSheet, TouchableOpacity, Text, FlatList} from "react-native";
 import {useNavigation} from "@react-navigation/native";
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
 import {RootStackParamList} from "../navigation/Root";
 import {COLORS} from "../themes/colors";
 import {useState} from "react";
+import {SearchingInput} from "../components/SearchingInput";
 
 interface ListItem {
   title: string,
@@ -12,65 +13,35 @@ interface ListItem {
 
 export const SelectLocation = () => {
   const { navigate } = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-  const [value, setValue] = useState('');
   const [list, setList] = useState<ListItem[]>([]);
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        placeholder={"Wpisz lokalizację"}
-        placeholderTextColor={COLORS.text}
-        selectionColor={COLORS.text}
-        style={styles.input}
-        onChangeText={setValue}
-        value={value}
-      />
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          setList(prevState => [...prevState, {title: value, value: value}]);
-          setValue('');
-        }}
-      >
-        <Text style={styles.buttonText}>Dodaj</Text>
-      </TouchableOpacity>
-      <View style={styles.listContainer}>
-      {list.map(item => (
+    <FlatList
+      ListHeaderComponent={<SearchingInput />}
+      data={list}
+      renderItem={({item}) => (
         <View style={styles.item}>
           <Text style={styles.itemText}>{item.title}</Text>
         </View>
-      ))}
-      </View>
-    </View>
+      )}
+    />
+    // <View style={styles.container}>
+
+
+    //   <View style={styles.listContainer}>
+    //   {list.map(item => (
+    //     <View style={styles.item}>
+    //       <Text style={styles.itemText}>{item.title}</Text>
+    //     </View>
+    //   ))}
+    //   </View>
+    // </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     margin: 10,
-  },
-  input: {
-    width: "100%",
-    borderWidth: 1,
-    borderColor: COLORS.link,
-    padding: 15,
-    paddingHorizontal: 20,
-    fontSize: 16,
-    color: COLORS.text,
-    borderRadius: 10,
-  },
-  button: {
-    width: "100%",
-    backgroundColor: COLORS.link,
-    marginTop: 20,
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonText: {
-    color: COLORS.text,
-    fontSize: 16,
   },
   listContainer: {
     width: "100%",
